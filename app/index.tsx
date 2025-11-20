997aa87d460adddc36014c57ad549046b72d7788
keimindev <keimindev@gmail.com>
2025-11-19 22:14:58 -0500

    [add] step 1 page

---
+++ app/index.tsx
@@ -0,0 +1,10 @@
+import { Text, View } from "react-native";
+
+export default function HomeScreen() {
+  return (
+    <View>
+      <Text style={{ color: 'black' }}>Home Screen</Text>
+      <Text style={{ color: 'black' }}>Home Screen</Text>
+    </View>
+  );
+}
