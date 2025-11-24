47970c6e32dae11c9433b3e67a214302bb223b77
keimindev <keimindev@gmail.com>
2025-11-24 06:18:46 -0500

    [fix] camera preview setup

--- app/CameraPage.tsx
+++ app/CameraPage.tsx
@@ -1,34 +1,94 @@
-import { Camera, CameraView } from "expo-camera";
-import * as MediaLibrary from "expo-media-library";
-import React, { useEffect, useState } from "react";
-import { StyleSheet, Text } from "react-native";
-import { SafeAreaView } from "react-native-safe-area-context";
+import { AntDesign } from '@expo/vector-icons';
+import { CameraType, CameraView, useCameraPermissions } from 'expo-camera';
+import { useRef, useState } from 'react';
+import { Button, StyleSheet, Text, TouchableOpacity, View } from 'react-native';
+import PhotoPreviewSection from '../components/PhotoPreviewSection';
 
 export default function CameraPage() {
-  const [isPermittedCamera, setIsPermittedCamera] = useState(false);
-  const getCameraPermission = async () => {
-    MediaLibrary.requestPermissionsAsync();
-    const cameraPermission = await Camera.requestMicrophonePermissionsAsync();
-    setIsPermittedCamera(cameraPermission.status === "granted");
-  };
+  const [facing, setFacing] = useState<CameraType>('back');
+  const [permission, requestPermission] = useCameraPermissions();
+  const [photo, setPhoto] = useState<any>(null);
+  const cameraRef = useRef<CameraView | null>(null);
 
-  useEffect(() => {
-    getCameraPermission();
-  }, []);
+  if (!permission) {
+    // Camera permissions are still loading.
+    return <View />;
+  }
+
+  if (!permission.granted) {
+    // Camera permissions are not granted yet.
+    return (
+      <View style={styles.container}>
+        <Text style={{ textAlign: 'center' }}>We need your permission to show the camera</Text>
+        <Button onPress={requestPermission} title="grant permission" />
+      </View>
+    );
+  }
+
+  function toggleCameraFacing() {
+    setFacing(current => (current === 'back' ? 'front' : 'back'));
+  }
+
+  const handleTakePhoto =  async () => {
+    if (cameraRef.current) {
+        const options = {
+            quality: 1,
+            base64: true,
+            exif: false,
+        };
+        const takedPhoto = await cameraRef.current.takePictureAsync(options);
+
+        setPhoto(takedPhoto);
+    }
+  }; 
+
+  const handleRetakePhoto = () => setPhoto(null);
+
+  if (photo) return <PhotoPreviewSection photo={photo} handleRetakePhoto={handleRetakePhoto} />
+
   return (
-    <SafeAreaView>
-      {!isPermittedCamera ? (
-        <Text>카메라 권한이 없습니다.</Text>
-      ) : (
-        <CameraView style={{ flex: 1 }} />
-      )}
-    </SafeAreaView>
+    <View style={styles.container}>
+      <CameraView style={styles.camera} facing={facing} ref={cameraRef}>
+        <View style={styles.buttonContainer}>
+          <TouchableOpacity style={styles.button} onPress={toggleCameraFacing}>
+            <AntDesign name='retweet' size={44} color='black' />
+          </TouchableOpacity>
+          <TouchableOpacity style={styles.button} onPress={handleTakePhoto}>
+            <AntDesign name='camera' size={44} color='black' />
+          </TouchableOpacity>
+        </View>
+      </CameraView>
+    </View>
   );
 }
 
 const styles = StyleSheet.create({
+  container: {
+    width: '100%',
+    height: '100%',
+    justifyContent: 'center',
+  },
   camera: {
-    width: "100%",
-    height: "100%",
+    width: '100%',
+    height: '80%'
+  },
+  buttonContainer: {
+    flex: 1,
+    flexDirection: 'row',
+    backgroundColor: 'transparent',
+    margin: 64,
+  },
+  button: {
+    flex: 1,
+    alignSelf: 'flex-end',
+    alignItems: 'center',
+    marginHorizontal: 10,
+    backgroundColor: 'gray',
+    borderRadius: 10,
+  },
+  text: {
+    fontSize: 24,
+    fontWeight: 'bold',
+    color: 'white',
   },
 });
